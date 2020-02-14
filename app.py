7ddff594ba281816a769eb21559512b8dc347054
rezan21 <reza.naghshineh@gmail.com>
2020-02-14 01:38:36 +0000

    render form pages

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegisterationForm, LoginForm
 app = Flask(__name__)
 
 posts = [
@@ -20,6 +21,13 @@ def home():
 def about():
     return render_template('about.html', title="ABOUT")
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegisterationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}.','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title="Register",form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
