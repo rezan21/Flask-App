8b7cb28b6d3b4a9008a78ac5309f3d776ecec8e4
rezan21 <reza.naghshineh@gmail.com>
2020-02-16 15:09:46 +0000

    updated

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True )
 posts = [
     {
         'author': 'Corey Schafer',
